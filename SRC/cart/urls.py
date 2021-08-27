cac7303c19fb8a5e0e3bdc01bb6e737172df2b77
miveh <imivechi@gmail.com>
2021-08-28 02:39:36 +0430

    more code

--- SRC/cart/urls.py
+++ SRC/cart/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from cart.views import CartView, add_to_cart, remove_from_cart, quantity
+from cart.views import CartView, add_to_cart, remove_from_cart, quantity, quantity_next_cart, NextCartView
 
 urlpatterns = [
     path('cart/', CartView.as_view(), name='cart'),
+    path('next_cart/', NextCartView.as_view(), name='next_cart'),
     path('add_to_cart/<slug>', add_to_cart, name='add_to_cart'),
     path('remove_from_cart/<slug>', remove_from_cart, name='cart_remove'),
     path('quantity/<slug>', quantity, name='quantity'),
+    path('quantity_next_cart/<slug>', quantity_next_cart, name='quantity_next_cart'),
 ]
--- SRC/cart/views.py
+++ SRC/cart/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView
@@ -5,7 +7,7 @@ from book.models import Book
 from cart.models import CartItems, Cart
 
 
-class CartView(ListView):
+class CartView(LoginRequiredMixin, ListView):
     """
     سبد خرید
     """
@@ -35,10 +37,30 @@ class CartView(ListView):
         """
 
         queryset = super(CartView, self).get_queryset()
-        queryset = queryset.get(user=self.request.user).cartitems_set.all()
+        queryset = queryset.get(user=self.request.user).cartitems_set.filter(ordered='O')
         return queryset
 
 
+class NextCartView(LoginRequiredMixin, ListView):
+    """
+    سبد خرید بعدی
+    """
+
+    model = Cart
+    template_name = 'cart/next_cart.html'
+    context_object_name = 'my_items'
+
+    def get_queryset(self):
+        """
+        :return: سفارشات درون سبد خرید کاربر
+        """
+
+        queryset = super(NextCartView, self).get_queryset()
+        queryset = queryset.get(user=self.request.user).cartitems_set.filter(ordered='U')
+        return queryset
+
+
+@login_required
 def add_to_cart(request, slug):
     """
     :param request: 😐
@@ -64,6 +86,32 @@ def add_to_cart(request, slug):
     return redirect('book_detail', slug=slug)
 
 
+def add_to_next_cart(request, slug):
+    allowed = False
+    book = get_object_or_404(Book, slug=slug)
+    cart_qs = CartItems.objects.filter(book=book, ordered='O', cart=Cart.objects.get(user=request.user))
+    if cart_qs.exists():
+        cart_item = cart_qs[0]
+        if cart_item.book.stock == 0:
+            allowed = True
+            cart_item.delete()
+            return render(request, 'cart/cart.html', {'book': book, 'allowed': allowed})
+
+        else:
+            is_ordered_item_qs = CartItems.objects.filter(book=book, ordered='O',
+                                                          cart=Cart.objects.filter(id=2)[0])  # must be change
+            if is_ordered_item_qs.exists():
+                is_ordered_item = is_ordered_item_qs[0]
+                is_ordered_item.quantity += cart_item.quantity
+                is_ordered_item.save()
+                cart_item.delete()
+            else:
+                cart_item.ordered = 'O'  # ordering
+                cart_item.save()
+
+    return redirect('orderedcart')
+
+
 def remove_from_cart(request, slug):
     """
     :param request: 😑
@@ -87,6 +135,7 @@ def quantity(request, slug):
     :param slug: -
     :return: سرجاش میمونه
     """
+
     if request.method == 'POST' and 'plus_quantity' in request.POST:
         book = get_object_or_404(Book, slug=slug)
         cart_qs = CartItems.objects.filter(book=book, ordered='O', cart=Cart.objects.get(user=request.user))
@@ -101,6 +150,39 @@ def quantity(request, slug):
     elif request.method == 'POST' and 'minus_quantity' in request.POST:
         book = get_object_or_404(Book, slug=slug)
         cart_qs = CartItems.objects.filter(book=book, ordered='O', cart=Cart.objects.get(user=request.user))
+        if cart_qs.exists():
+            cart_item = cart_qs[0]
+            if cart_item.quantity <= 1:
+                pass
+            else:
+                cart_item.quantity -= 1
+                cart_item.save()
+
+    return redirect('cart')
+
+
+def quantity_next_cart(request, slug):
+    """
+    افزایش تعداد دلخواه از کتاب در سبد خرید به شرط داشتن موجودی کافی از کتاب کاهش تعداد دلخواه از کتاب در سبد خرید تا رسیدن به تعداد 1
+    :param request: -
+    :param slug: -
+    :return: سرجاش میمونه
+    """
+
+    if request.method == 'POST' and 'plus_quantity' in request.POST:
+        book = get_object_or_404(Book, slug=slug)
+        cart_qs = CartItems.objects.filter(book=book, ordered='U', cart=Cart.objects.get(user=request.user))
+        if cart_qs.exists():
+            cart_item = cart_qs[0]
+            if cart_item.quantity >= cart_item.book.stock:
+                pass
+            else:
+                cart_item.quantity += 1
+                cart_item.save()
+
+    elif request.method == 'POST' and 'minus_quantity' in request.POST:
+        book = get_object_or_404(Book, slug=slug)
+        cart_qs = CartItems.objects.filter(book=book, ordered='U', cart=Cart.objects.get(user=request.user))
         if cart_qs.exists():
             cart_item = cart_qs[0]
             if cart_item.quantity <= 1:
