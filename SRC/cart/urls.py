8964a359daf82bbf822c920e441185f125fe527f
miveh <imivechi@gmail.com>
2021-08-28 01:46:56 +0430

    more code

--- SRC/cart/urls.py
+++ SRC/cart/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from cart.views import CartView, add_to_cart
+from cart.views import CartView, add_to_cart, remove_from_cart, quantity
 
 urlpatterns = [
     path('cart/', CartView.as_view(), name='cart'),
     path('add_to_cart/<slug>', add_to_cart, name='add_to_cart'),
+    path('remove_from_cart/<slug>', remove_from_cart, name='cart_remove'),
+    path('quantity/<slug>', quantity, name='quantity'),
 ]
--- SRC/cart/views.py
+++ SRC/cart/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView
-
 from book.models import Book
 from cart.models import CartItems, Cart
 
@@ -14,6 +13,21 @@ class CartView(ListView):
     model = Cart
     template_name = 'cart/cart.html'
     context_object_name = 'my_items'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        """
+        :param object_list: -
+        :param kwargs: -
+        :return: قیمت نهایی فاکتور خرید
+        """
+
+        total_price_factor = 0
+        for item in CartItems.objects.filter(cart=Cart.objects.get(user=self.request.user), ordered='O'):
+            total_price_factor += item.total_price
+
+        context = super().get_context_data(**kwargs)
+        context['total_price_factor'] = total_price_factor
+        return context
 
     def get_queryset(self):
         """
@@ -33,7 +47,7 @@ def add_to_cart(request, slug):
     """
 
     book = get_object_or_404(Book, slug=slug)
-    cart_qs = CartItems.objects.filter(book=book, ordered='U',
+    cart_qs = CartItems.objects.filter(book=book, ordered='O',
                                        cart=Cart.objects.get(user=request.user))
 
     if cart_qs.exists():
@@ -44,7 +58,55 @@ def add_to_cart(request, slug):
             cart_item.quantity += 1
             cart_item.save()
     else:
-        cart_item = CartItems.objects.create(book=book, ordered='U', cart=Cart.objects.get(user=request.user))
+        cart_item = CartItems.objects.create(book=book, ordered='O', cart=Cart.objects.get(user=request.user))
         cart_item.save()
 
     return redirect('book_detail', slug=slug)
+
+
+def remove_from_cart(request, slug):
+    """
+    :param request: 😑
+    :param slug: اسلاگ کتاب
+    :return: سرجاش توی سبد خرید بمونه
+    """
+
+    book = get_object_or_404(Book, slug=slug)
+    cart_qs = CartItems.objects.filter(book=book, cart=Cart.objects.get(user=request.user))
+    if cart_qs.exists():
+        cart_item = cart_qs[0]
+        cart_item.delete()
+
+    return redirect('cart')
+
+
+def quantity(request, slug):
+    """
+    افزایش تعداد دلخواه از کتاب در سبد خرید به شرط داشتن موجودی کافی از کتاب کاهش تعداد دلخواه از کتاب در سبد خرید تا رسیدن به تعداد 1
+    :param request: -
+    :param slug: -
+    :return: سرجاش میمونه
+    """
+    if request.method == 'POST' and 'plus_quantity' in request.POST:
+        book = get_object_or_404(Book, slug=slug)
+        cart_qs = CartItems.objects.filter(book=book, ordered='O', cart=Cart.objects.get(user=request.user))
+        if cart_qs.exists():
+            cart_item = cart_qs[0]
+            if cart_item.quantity >= cart_item.book.stock:
+                pass
+            else:
+                cart_item.quantity += 1
+                cart_item.save()
+
+    elif request.method == 'POST' and 'minus_quantity' in request.POST:
+        book = get_object_or_404(Book, slug=slug)
+        cart_qs = CartItems.objects.filter(book=book, ordered='O', cart=Cart.objects.get(user=request.user))
+        if cart_qs.exists():
+            cart_item = cart_qs[0]
+            if cart_item.quantity <= 1:
+                pass
+            else:
+                cart_item.quantity -= 1
+                cart_item.save()
+
+    return redirect('cart')
